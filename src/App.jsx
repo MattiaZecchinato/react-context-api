6b7f14e2641d14dbeb0d759300f021f5a2e8d00b
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-23 14:44:54 +0200

    feat: define posts list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+//pages
+import PostsPage from './pages/PostsPage';
+
 function App() {
+
+  const [postsList, setPostsList] = useState([]);
+
+  const uri = 'https://jsonplaceholder.typicode.com/posts';
+
+  useEffect(() => {
+
+    axios.get(uri)
+    .then(res => setPostsList(res.data))
+    .catch(err => console.error(err))
+  }, [])
 
   return (
     <>
-     
+      <PostsPage listPosts={postsList}/>
     </>
   )
 }
